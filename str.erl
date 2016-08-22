ab22795cdef22f72450e09e94667ec0e236455fb
Yaroslav Dashkevich <jaroslav.dashkevich@gmail.com>
2016-08-22 13:36:09 +0700

    Fix pattern tree construction

--- str.erl
+++ str.erl
@@ -3,33 +3,23 @@
 
 -compile(export_all).
 
-patternInsert([], Node) -> io:format("3~p~n", Node),Node;
-% patternInsert([H], []) ->
-% 	io:format("3~p~n", H),
-% 	[{H, []}];
-% patternInsert([H|T], []) ->
-% 	io:format("4~p~n", H),
-% 	[{H, patternInsert(T, [])}];
-patternInsert([H], Node) ->
-	io:format("1~p~n", H),
-	{Ls, _, Gt} = split(H, Node),
-	Ls ++ [{H, []}] ++ Gt;
+patternInsert([], Node)
+	when length(Node) == 0 -> [];
+
+patternInsert([], Node) -> Node;
+
+% patternInsert([H|T], []) -> [{H, patternInsert(T, emptyNode())}].
+
+patternInsert([H|T], Node)
+	when length(Node) == 0 -> [{H, patternInsert(T, emptyNode())}];
+
 patternInsert([H|T], Node) ->
-	{Ls, Eq, Gt} = split(H, Node),
-	io:format("2~p~n", H),
-	case Eq of
-		[] 					 	 -> Ls ++ [{H, patternInsert(T, [])}] ++ Gt;
-		[{Val, NNode}] -> Ls ++ [{Val, patternInsert(T, NNode)}] ++ Gt
+	Out = lists:filter(fun ({Val, _}) -> Val =/= H end, Node),
+	case length(Out) == length(Node) of
+		true  -> Out ++ [{H, patternInsert(T, emptyNode())}];
+		false ->
+			[{_, OldNode}|_] = lists:filter(fun ({Val, _}) -> Val == H end, Node),
+			Out ++ [{H, patternInsert(T, OldNode)}]
 	end.
 
-split (_, []) -> {[],[],[]};
-split (H, Node) -> split(H, Node, {[],[],[]}).
-split (_, [], Acc) -> Acc;
-split (H, [{Val, Node}|T], {Vl, [], Vg})
-	when H == Val -> split(H, T, {Vl, [{Val, Node}], Vg});
-split (H, [{Val, Node}|T], {Vl, Ve, Vg})
-	when H > Val  -> split(H, T, {[{Val, Node}|Vl], Ve, Vg});
-split (H, [{Val, Node}|T], {Vl, Ve, Vg})
-	when H < Val  -> split(H, T, {Vl, Ve, [{Val, Node}|Vg]}).
-
-emptyNode () -> nil.
+emptyNode () -> [].

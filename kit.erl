75604d81e42a0fc3667f60e7c0a18663377e3579
Yaroslav Dashkevich <jaroslav.dashkevich@gmail.com>
2016-09-30 15:27:16 +0700

    Add timeouts

--- kit.erl
+++ kit.erl
@@ -43,20 +43,32 @@ putIntoFridge( Food, FoodList ) -> fridge2( [ Food | FoodList ] ).
 
 store( Pid, Item ) ->
   Pid ! { self(), {store, Item} },
-  receive
-    {Pid, Msg} -> Msg
+  receive {Pid, Msg} -> Msg
+  after 3000 -> timeout
   end.
 
 take( Pid, Item ) ->
   Pid ! { self(), {take, Item} },
-  receive
-    {Pid, Msg} -> Msg
+  receive {Pid, Msg} -> Msg
+  after 3000 -> timeout
   end.
 
 list( Pid ) ->
   Pid ! { self(), list },
-  receive
-    Msg -> Msg
+  receive Msg -> Msg
+  after 3000 -> timeout
   end.
 
 createFridge( FoodList ) -> spawn( ?MODULE, fridge2, [FoodList] ).
+
+
+important() ->
+  receive { Priority, Message }
+    when Priority > 10 -> [ Message | important() ]
+  after 0 -> normal()
+  end.
+
+normal() ->
+  receive { _, Message } -> [ Message | normal() ]
+  after 0 -> []
+  end.

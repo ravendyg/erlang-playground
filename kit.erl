16d95b075fb1eb7bfaa64fde1f51c02b99ed5951
Yaroslav Dashkevich <jaroslav.dashkevich@gmail.com>
2016-09-30 15:15:04 +0700

    Improve fridge

--- kit.erl
+++ kit.erl
@@ -18,7 +18,7 @@ fridge2( FoodList ) ->
   receive
     {From, {store, Food}} ->
       From ! {self(), ok},
-      fridge2( [Food | FoodList] );
+      putIntoFridge( Food, FoodList );
     {From, {take, Food}} ->
       case lists:member(Food, FoodList) of
         true ->
@@ -34,3 +34,29 @@ fridge2( FoodList ) ->
     terminate ->
       ok
   end.
+
+putIntoFridge( Food, FoodList )
+  when is_list( Food ) == true -> fridge2( Food ++ FoodList );
+
+putIntoFridge( Food, FoodList ) -> fridge2( [ Food | FoodList ] ).
+
+
+store( Pid, Item ) ->
+  Pid ! { self(), {store, Item} },
+  receive
+    {Pid, Msg} -> Msg
+  end.
+
+take( Pid, Item ) ->
+  Pid ! { self(), {take, Item} },
+  receive
+    {Pid, Msg} -> Msg
+  end.
+
+list( Pid ) ->
+  Pid ! { self(), list },
+  receive
+    Msg -> Msg
+  end.
+
+createFridge( FoodList ) -> spawn( ?MODULE, fridge2, [FoodList] ).

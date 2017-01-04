9c6808c45f5b2f5ff72a5c87559263bfd370e83e
Yaroslav <jaroslav.dashkevich@gmail.com>
2017-01-04 20:24:43 +0700

    Lesson 09 - stage 04 (add API)

--- h09.erl
+++ h09.erl
@@ -9,22 +9,43 @@ start() ->
 
 loop(State) ->
   receive
-    {add, Item} ->
-      io:format("~p adds ~p to its state ~n", [self(), Item]),
+    {add, From, Item} ->
       NewState = [Item | State],
+      From ! {reply, ok},
       ?MODULE:loop(NewState);
-    {remove, Item}  ->
-      NewState =
+    {remove, From, Item}  ->
+      {Reply, NewState} =
         case lists:member(Item, State) of
-          true  -> lists:delete(Item, State);
-          false ->
-            io:format("Have no ~p~n", [Item]),
-            State
+          true  -> {ok, lists:delete(Item, State)};
+          false -> {{error, not_exists}, State}
         end,
-        ?MODULE:loop(NewState);
-    show_items ->
-      io:format("my items are ~p~n", [State]),
+      From ! {reply, Reply},
+      ?MODULE:loop(NewState);
+    {show_items, From} ->
+      From ! {reply, State},
       ?MODULE:loop(State);
     stop -> io:format("~p stoped ~n", [self()]);
     _    -> ?MODULE:loop(State)
+  end.
+
+add_item(Pid, Item) ->
+  Pid ! {add, self(), Item},
+  handle_reply().
+
+remove_item(Pid, Item) ->
+  Pid ! {remove, self(), Item},
+  handle_reply().
+
+show_items(Pid) ->
+  Pid ! {show_items, self()},
+  handle_reply().
+
+stop(Pid) ->
+  Pid ! stop,
+  ok.
+
+handle_reply() ->
+  receive
+    {reply, Reply} -> Reply
+  after 5000 -> timeout
   end.

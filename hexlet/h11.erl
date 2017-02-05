d2c0fe50942c77a795224933088a2fef0953c22d
Yaroslav <jaroslav.dashkevich@gmail.com>
2017-02-05 13:31:41 +0700

    Hexlet: Initial task 11

---
+++ hexlet/h11.erl
@@ -0,0 +1,58 @@
+-module(h11).
+
+% -export([run/0, run_and_crash/0, work/1, work_and_crash_one/1]).
+-compile(export_all).
+
+%% sample 1
+
+run() ->
+    [spawn_link(?MODULE, work, [Id]) || Id <- lists:seq(0, 5)],
+    ok.
+
+work(Id) ->
+    io:format("~p ~p started~n", [Id, self()]),
+    timer:sleep(1000),
+    io:format("~p ~p stopped~n", [Id, self()]),
+    ok.
+
+run_and_crash() ->
+    [spawn_link(?MODULE, work_and_crash_one, [Id]) || Id <- lists:seq(0, 5)],
+    ok.
+
+work_and_crash_one(Id) ->
+    io:format("~p ~p started~n", [Id, self()]),
+    if
+        Id == 3 ->
+            io:format("~p ~p exiting~n", [Id, self()]),
+            exit(for_some_reason);
+            % ok;
+        true -> ok
+    end,
+    timer:sleep(1000),
+    io:format("~p ~p stopped~n", [Id, self()]),
+    ok.
+
+
+%% sample 2
+
+run2() ->
+    spawn(fun system_process/0),
+    ok.
+
+
+system_process() ->
+    io:format("~p system process started~n", [self()]),
+    process_flag(trap_exit, true),
+    spawn_link(fun worker/0),
+    receive
+        Msg -> io:format("~p system process got message ~p~n", [self(), Msg])
+    after 2000 -> ok
+    end,
+    ok.
+
+
+worker() ->
+    io:format("~p worker started~n", [self()]),
+    timer:sleep(500),
+    % exit(some_reason),
+    ok.
---
+++ hexlet/monitor/main.erl
@@ -0,0 +1,9 @@
+-module(main).
+
+-export([parse/1]).
+
+
+parse(Files) ->
+%% BEGIN (write your solution here)
+
+%% END

19ac848016040ea8d689d3aa12d93e9754a2d697
Yaroslav <jaroslav.dashkevich@gmail.com>
2017-02-11 19:48:23 +0700

    Hexlet: Impelement 'encode'

--- hexlet/my_crypt/src/my_crypt.erl
+++ hexlet/my_crypt/src/my_crypt.erl
@@ -3,9 +3,15 @@
 -behaviour(application).
 
 -export([start/2, stop/1]).
+-export([encode/1]).
 
 
 start(_StartType, _StartArgs) ->
   my_crypt_sup:start_link().
 
 stop(_) -> ok.
+
+
+encode(Input) ->
+  my_crypt_worker:encode(Input).
+
--- hexlet/my_crypt/src/my_crypt_worker.erl
+++ hexlet/my_crypt/src/my_crypt_worker.erl
@@ -1,7 +1,7 @@
 -module(my_crypt_worker).
 -behavior(gen_server).
 
--export([start_link/0]).
+-export([start_link/0, encode/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 
@@ -12,12 +12,29 @@ start_link() ->
 init([]) ->
   {ok, no_state}.
 
+%% API
+encode(Input) ->
+  gen_server:call(?MODULE, {encode, Input}).
+
+
+%% Helpers
+encode(<<>>, _, Acc) -> Acc;
+encode(Input, <<>>, Acc) -> encode(Input, get_key(), Acc);
+
+encode(<<In:8, RestIn/binary>>, <<Key:8, RestKey/binary>>, Acc) ->
+  encode(RestIn, RestKey, <<Acc/bytes, (In bxor Key):8>>).
+
+get_key() ->
+  {ok, Key} = application:get_env(my_crypt, crypt_key),
+  Key.
+
+
+%% sync
+handle_call({encode, Input}, _From, State) ->
+  {reply, encode(Input, get_key(), <<>>), State}.
 
 
 %%  plugs
-handle_call(_, _, Id) ->
-  {reply, ok, Id}.
-
 handle_cast(_, Id) ->
   {noreply, Id}.
 

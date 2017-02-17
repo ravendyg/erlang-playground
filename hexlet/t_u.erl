b569b8da7608a330b1774493d8983799b7cd00af
Yaroslav Dashkevich <jaroslav.dashkevich@gmail.com>
2017-02-17 11:22:55 +0700

    Deleting aaaa

--- hexlet/t_u.erl
+++ hexlet/t_u.erl
@@ -12,7 +12,7 @@ start(Port) ->
 
 server(Port) ->
   io:format("server on port ~p~n", [Port]),
-  {ok, ListenSocket} = gen_tcp:listen(Port, [binary, {active, true}]),
+  {ok, ListenSocket} = gen_tcp:listen(Port, [binary, {active, false}, {packet, raw}]),
   [spawn(?MODULE, accept, [Id, ListenSocket]) || Id <- lists:seq(1, 5)],
   receive
     stop -> gen_tcp:close(ListenSocket)
@@ -21,32 +21,39 @@ server(Port) ->
 
 accept(Id, ListenSocket) ->
   io:format("Socket #~p wait for client~n", [Id]),
-  {ok, _Socket} = gen_tcp:accept(ListenSocket),
+  {ok, Socket} = gen_tcp:accept(ListenSocket),
   io:format("Socket #~p, session started~n", [Id]),
-  handle_connection(Id, ListenSocket).
+  handle_connection(Id, ListenSocket, Socket).
 
-handle_connection(Id, ListenSocket) ->
-  handle_connection(Id, ListenSocket, null).
 
-handle_connection(Id, ListenSocket, Connection)->
-  receive
-    {tcp, Socket, Msg} ->
+handle_connection(Id, ListenSocket, Socket)->
+  case gen_tcp:recv(Socket, 0) of
+    {ok, Msg} ->
       io:format("Socket #~p got message: ~p~n", [Id, binary:split(Msg, [<<"\r\n">>], [global])]),
       gen_tcp:send(Socket, Msg),
       handle_connection(Id, ListenSocket, Socket);
-    {tcp_closed, _Socket} ->
+    {error, closed} ->
       io:format("Socket #~p, session closed ~n", [Id]),
       accept(Id, ListenSocket)
-    after 5000 ->
-      case Connection of
-        null ->
-          handle_connection(Id, ListenSocket, null);
-        _    ->
-          gen_tcp:send(Connection, ""),
-          gen_tcp:close(Connection),
-          handle_connection(Id, ListenSocket, null)
-      end
     end.
+  % receive
+  %   {tcp, Socket, Msg} ->
+  %     io:format("Socket #~p got message: ~p~n", [Id, binary:split(Msg, [<<"\r\n">>], [global])]),
+  %     gen_tcp:send(Socket, Msg),
+  %     handle_connection(Id, ListenSocket, Socket);
+  %   {tcp_closed, _Socket} ->
+  %     io:format("Socket #~p, session closed ~n", [Id]),
+  %     accept(Id, ListenSocket)
+  %   after 5000 ->
+  %     case Connection of
+  %       null ->
+  %         handle_connection(Id, ListenSocket, null);
+  %       _    ->
+  %         gen_tcp:send(Connection, ""),
+  %         gen_tcp:close(Connection),
+  %         handle_connection(Id, ListenSocket, null)
+  %     end
+  %   end.
 
 % stop(Pid) ->
 %   Pid ! close.
